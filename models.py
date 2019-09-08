5724002b6554fca9a82f424254f7922a86fe757e
Reem <19894381+Reemr@users.noreply.github.com>
2019-09-08 18:08:27 -0400

    added two new models

--- models.py
+++ models.py
@@ -3,17 +3,59 @@ from keras import layers
 from keras import models
 from keras import engine
 from keras.layers import Input, Conv2D, MaxPooling2D, BatchNormalization, Dense
-from keras.layers import Dropout, Flatten
+from keras.layers import Dropout, Flatten, GRU
 from keras.models import Model
+from keras.layers.merge import Average, Maximum, Add
+from keras.applications.resnet50 import ResNet50
 
 CLASSES = 4
 IMG_SIZE = (216,216)
 SEQ = 10
 BATCH_SIZE = 28
 
+'''
+def RNN(in_shape), weights_dir):
 
-def CNN():
-    in_shape = (IMG_SIZE[0],IMG_SIZE[1],3)
+    input_img = Input(shape=in_shape)
+
+    x = GRU(128)(input_img)
+    x = Dropout(0.9)(x)
+    x = Dense(CLASSES, activation='softmax')(x)
+
+    model = Model(inputs=input_img, outputs=x)
+
+
+    if os.path.exists(weights_dir):
+        model.load_weights(weights_dir)
+
+
+    return model
+'''
+
+def finetuned_resnet(include_top, weights_dir, in_shape):
+
+    base_model = ResNet50(include_top=False, weights='imagenet', input_shape=in_shape)
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    x = base_model.output
+    x = Flatten()(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+
+    if include_top:
+        x = Dense(CLASSES, activation='softmax')(x)
+
+    model = Model(inputs=base_model.input, outputs=x)
+    if os.path.exists(weights_dir):
+        model.load_weights(weights_dir, by_name=True)
+
+    return model
+
+def CNN(in_shape, weights_dir):
+    #in_shape = (IMG_SIZE[0],IMG_SIZE[1],3)
 
     input_img = Input(shape=in_shape)
 
@@ -44,9 +86,29 @@ def CNN():
 
     model = Model(inputs=input_img, outputs=x)
 
+    if os.path.exists(weights_dir):
+        model.load_weights(weights_dir)
+
     return model
 
+def two_stream_model(spatial_weights, temporal_weights):
+
+    spatial_stream = finetuned_resnet(include_top=True, weights_dir=spatial_weights)
+
+    temporal_stream = CNN(input_shape, temporal_weights)
+
+    spatial_output = spatial_stream.output
+    temporal_output = temporal_stream.output
+
+    fused_output = Average(name='fusion_layer')([spatial_output, temporal_output])
+
+    model = Model(inputs=[spatial_stream.input, temporal_stream.input],
+     outputs=fused_output, name=two_stream)
+
+    return model
 
 if __name__ == '__main__':
-    Model = CNN()
-    print(Model.summary())
+
+    sha = (IMG_SIZE[0],IMG_SIZE[1])
+    m = CNN(sha)
+    print(m.summary())
